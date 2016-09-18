f9e2575efd25c66c6e41d42b071d267eb7ea97e5
Jonathan Xu <jonathanj.xu@yahoo.com>
2016-09-17 23:54:49 -0500

    Inisght for retrieving logs

--- routes/api/insights.js
+++ routes/api/insights.js
@@ -4,11 +4,12 @@ var router = express.Router();
 var Log = require('../../models/Log');
 
 router.get('/logs', function(req, res, next) {
-  Log.find(function(err, post) {
-    if (err)
-      return next(err);
-    res.json(post);
-  });
+  Log.find()
+    .populate('location')
+    .exec(function(err, post) {
+      if (err) return next(err);
+      res.json(post);
+    });;
 });
 
 module.exports = router;
